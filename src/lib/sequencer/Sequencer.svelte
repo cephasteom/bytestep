ce30282c88a7a8ac1c853bcfb10b5b4b87911459
Peter Thomas <petethomasmusique@gmail.com>
2026-01-08 22:48:19 +0000

    main:

--- src/lib/sequencer/Sequencer.svelte
+++ src/lib/sequencer/Sequencer.svelte
@@ -1,17 +1,21 @@
 <script lang="ts">
     const rows = 24;
     const cols = 32;
-
     let currentRow = 2;
 
     const cells = Array(rows).fill(null).map(() => Array(cols).fill(false));
+
+    let collapsed = false;
+    const toggle = () => collapsed = !collapsed;
 </script>
 
 <section 
-    class="sequencer"   
-    role="application"
-    on:mouseleave={() => currentRow = -1}
+    class="sequencer"
+    class:sequencer--collapsed={collapsed}
 >
+    <div class="sequencer__meta">
+        <button on:click={toggle}>{collapsed ? '▲' : '▼'}</button> 
+    </div>
     <div class="sequencer__piano">
         {#each Array(rows) as _, rowIndex}
             <div 
@@ -22,7 +26,11 @@
             ></div>
         {/each}
     </div>
-    <div class="sequencer__grid">
+    <div 
+        class="sequencer__grid"
+        role="application"
+        on:mouseleave={() => currentRow = -1}
+    >
         {#each Array(cols) as _, colIndex}
             {#each Array(rows) as _, rowIndex}
                 <button 
@@ -31,7 +39,11 @@
                     data-row={rowIndex}
                     style="grid-column: {colIndex + 1}; grid-row: {rowIndex + 1};}"
                     class:sequencer__cell--highlighted={!(Math.floor(colIndex / 4) % 2)}
-                    class:sequencer__cell--active={cells[rowIndex][colIndex]}
+                    class:sequencer__cell--active={
+                        collapsed
+                            ? cells.some(row => row[colIndex]) && rowIndex === 0
+                            : cells[rowIndex][colIndex]
+                    }
                     aria-label="Toggle cell at row {rowIndex + 1}, column {colIndex + 1}"
                     on:click={() => cells[rowIndex][colIndex] = !cells[rowIndex][colIndex]}
                     on:mouseover={() => currentRow = rowIndex}
@@ -48,7 +60,29 @@
         border: 1px solid white;
         border-radius: 2px;
         display: grid;
-        grid-template-columns: auto 1fr;
+        grid-template-columns: auto auto 1fr;
+        max-height: calc(24 * 1.5rem);
+        transition: max-height 0.3s ease;
+        overflow: hidden;
+
+        &--collapsed {
+            transition: max-height 0.3s ease;
+            max-height: 1.5rem;
+        }
+
+        &__meta {
+            width: 3rem;
+
+            & button {
+                width: 100%;
+                height: 1.5rem;
+                background: none;
+                border: none;
+                color: white;
+                font-size: 1rem;
+                cursor: pointer;
+            }
+        }
 
         &__piano {
             display: grid;
